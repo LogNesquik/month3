e4c703b263d66902e0c8d18e965626cf02992d70
LogNesquik <logenerlogenerovic@gmail.com>
2026-01-06 21:35:16 +0600

    Сделал третью классную работу.

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+# pip install flet[all]
 import flet as ft
 
 def main(page: ft.Page):
-    text_hello = ft.Text(value="HELLO WORLD")
+    page.title = 'My first Flet app'
+    page.theme_mode = ft.ThemeMode.LIGHT
+    text_hello = ft.Text(value="HELLO WORLD", color =ft.Colors.BLUE)
 
-    text_button = ft.TextButton("SEND")
-    elevated_button = ft.ElevatedButton("SEND")
+    text_hello.value = 'Hello'
 
-    page.add(text_hello, text_button, elevated_button)
+    def on_button_click(e):
+        name = name_input.value.strip()
+        print(name)
+        if name:
+            text_hello.value = f'Hello, {name}!'
+            text_hello.color = ft.Colors.GREEN
+            name_input.value = None
+        else:
+            text_hello.value = "Введите корректное имя!"
+            text_hello.color = ft.Colors.RED
+            name_input.value = None
+        page.update()
+        # page.update()
+
+    elevated_button = ft.ElevatedButton("SEND", icon=ft.Icons.SEND, on_click=on_button_click)
+    name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
+
+    page.add(text_hello, name_input, elevated_button)
+
 
 ft.app(target=main)
 # ft.run(target=main)
